c50ce6d518640aeceb4dfb4ff8d01e67f33969c6
zhichao pan <pan.z@husky.neu.edu>
2019-01-30 18:37:18 -0500

    feat: complete token-based authentication

--- webapp/filters/basic_auth.js
+++ webapp/filters/basic_auth.js
@@ -1,8 +1,18 @@
 const { users } = require("./../mock");
 const { checkPwd } = require("./../services/user");
+const { sign, verify } = require("./../services/jwt");
 
 async function basicAuth(ctx, next) {
   const { request, response } = ctx;
+
+  const token = ctx.cookies.get("token");
+  const decodedToken = verify(token);
+  console.log(decodedToken instanceof Error);
+  if (!(decodedToken instanceof Error)) {
+    next();
+    return;
+  }
+
   if (
     !request.headers.authorization ||
     request.headers.authorization.indexOf("Basic ") === -1
@@ -18,16 +28,18 @@ async function basicAuth(ctx, next) {
   );
   const [username, password] = credentials.split(":");
 
-  const flag = users.filter(
+  const getUser = users.filter(
     async user =>
       user.username == username && (await checkPwd(password, user.password))
   );
-  // console.log(flag);
-  if (!flag.length) {
+
+  if (!getUser.length) {
     response.status = 401;
     response.body = { message: "Invalid Authentication Credentials" };
     return;
   }
+
+  ctx.cookies.set("token", sign(getUser[0].id, getUser[0].username));
 
   next();
 }
--- webapp/mock.js
+++ webapp/mock.js
@@ -1,9 +1,11 @@
 const users = [
   {
+    id: "1",
     username: "k@husky.neu.edu",
     password: "!Pa1990528"
   },
   {
+    id: "2",
     username: "kk@husky.neu.edu",
     password: "$2b$10$PoOCaSbQdrFggN3N90FBTuKw5vR88lvHmoY5w10LDpqYnQNEgdRui"
   }
