0a0514131be0aa2c7fef5fc781d8717451be25be
Mandy Wang <wangmychn@gmail.com>
2019-01-31 01:07:20 -0500

    test

--- webapp/test/routes.test.js
+++ webapp/test/routes.test.js
@@ -1,12 +1,40 @@
 const server = require("./../index");
 const request = require("supertest");
 const test = require("ava");
+
 test.afterEach(() => {
   server.close();
 });
-test("routes: index", async t => {
-  const response = await request(server).get("/");
-  t.is(response.status, 401);
-  t.is(response.type, "application/json");
+
+test("GET: Success", async t => {
+  await request(server)
+      .get("/")
+      .auth('username', 'password')
+      .expect(200)
   t.pass();
 });
+
+test("GET: Faild", async t => {
+  await request(server)
+      .get("/")
+      .expect(401);
+  t.pass();
+});
+
+test("POST: Success", async t => {
+  await request(server)
+      .post("/user/register")
+      .send({username: '123@gmail.com', pwd: '1234Aa56'})
+      .expect(200)
+  t.pass();
+});
+
+/*test("POST: Faild", async t => {
+  await request(server)
+      .post("/user/register")
+      .set('Authorization', 'token')
+      .send({ pwd: '12345' })
+      .expect(403)
+  t.pass();
+});
+*/
