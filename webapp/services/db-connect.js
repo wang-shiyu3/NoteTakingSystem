8a70722971e0464d3b48e90de21cb6737551e547
zhichao pan <pan.z@husky.neu.edu>
2019-01-31 17:12:53 -0500

    chore: disable sql logging

--- webapp/services/db-connect.js
+++ webapp/services/db-connect.js
@@ -5,28 +5,29 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize("csye6225", "admin", "xxxxxx", {
   host: "3.17.36.232",
   port: 3306,
-  dialect: "mysql"
+  dialect: "mysql",
+  logging: false
 });
 
-//Checking connection status
-sequelize
-  .authenticate()
-  .then(function(err) {
-    if (err) {
-      console.log("There is connection in ERROR");
-    } else {
-      // sequelize.query("delete from users").then(function(rows) {
-      //   console.log(JSON.stringify(rows));
-      // });
+// Checking connection status
+// sequelize
+//   .authenticate()
+//   .then(function(err) {
+//     if (err) {
+//       console.log("There is connection in ERROR");
+//     } else {
+//       // sequelize.query("delete from users").then(function(rows) {
+//       //   console.log(JSON.stringify(rows));
+//       // });
 
-      sequelize.query("select * from users").then(function(rows) {
-        console.log(JSON.stringify(rows));
-      });
-      console.log("Connection has been established successfully");
-    }
-  })
-  .catch(err => {
-    console.log(err);
-  });
+//       sequelize.query("select * from users").then(function(rows) {
+//         console.log(JSON.stringify(rows));
+//       });
+//       console.log("Connection has been established successfully");
+//     }
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
 
 module.exports = sequelize;
