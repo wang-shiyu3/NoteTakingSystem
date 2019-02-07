5b8782679a9c2349da3b7ef9e7586b6b23e83072
zhichao pan <pan.z@husky.neu.edu>
2019-02-07 17:12:44 -0500

    feat: update create and delete

--- infrastructure/aws/cloudformation/csye6225-aws-networking-setup.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-networking-setup.sh
@@ -1,16 +1,69 @@
 #!/bin/bash
 # create-aws-vpc
 
-#variables used in script:
+# variables used in script:
 
-vpcName="$name VPC"
-subnetName="$name Subnet"
-gatewayName="$name Gateway"
-routeTableName="$name Route Table"
-securityGroupName="$name Security Group"
-vpcCidrBlock="10.0.0.0/16"
-subNetCidrBlock="10.0.1.0/24"
-port22CidrBlock="0.0.0.0/0"
-destinationCidrBlock="0.0.0.0/0"
+# echo "\nPlease select number to run task.
+# 1: Create a Virtual Private Cloud (VPC)
+# 2: Create subnets in your VPC.
+# 3: Create Internet Gateway resource
+# 4: Attach the Internet Gateway to the created VPC.
+# 5: Create a public Route Table. Attach all subnets created above to the route table.
+# "
+
+
+
+echo "Enter vpc-cidr-block and press [ENTER]: "
+read vpcCidrBlock
 
 echo "Creating VPC..."
+# create vpc with cidr block /16
+aws_response=$(aws ec2 create-vpc \
+ --cidr-block "$vpcCidrBlock" \
+ --output json)
+echo $aws_response
+
+echo "Enter the vpc-id shown above to store it and press [ENTER]: "
+read vpcID
+
+# create subnet for vpc
+# Show availability zones
+aws ec2 describe-availability-zones
+
+create_subnet () {
+  echo "Enter availability-zone and press [ENTER]: "
+  read availabilityZone
+  echo "Enter sub-net-cidr-block and press [ENTER]: "
+  read subNetCidrBlock
+  aws ec2 create-subnet \
+    --cidr-block "$subNetCidrBlock" \
+    --availability-zone "$availabilityZone" \
+    --vpc-id "$vpcID" \
+    --output json
+}
+
+declare -a arr=("first" "second" "third")
+for i in "${arr[@]}"
+do
+   echo "Create $i subnet"
+   create_subnet
+done
+
+echo "Creating internet-gateway ..."
+aws ec2 create-internet-gateway
+echo "Enter the gateway-id shown above to store it and press [ENTER]: "
+read internetGatewayId
+
+aws ec2 attach-internet-gateway --vpc-id $vpcID --internet-gateway-id $internetGatewayId
+
+echo "Creating route-table ..."
+aws ec2 create-route-table --vpc-id $vpcID
+echo "Enter the RouteTableId shown above to store it and press [ENTER]: "
+read routeTableId
+
+aws ec2 create-route --route-table-id $routeTableId --destination-cidr-block 0.0.0.0/0 --gateway-id $internetGatewayId
+
+
+echo "\n"
+echo "All set"
+exit 0
---
+++ infrastructure/aws/cloudformation/csye6225-aws-networking-teardown.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# delete-aws-vpc
+
+aws ec2 describe-vpcs
+
+echo "Enter your vpc-id and press [ENTER]: "
+read vpcId
+# rmove vpc with cidr block /16
+
+# Delete subnets
+for i in `aws ec2 describe-subnets --filters Name=vpc-id,Values="${vpcId}" | grep subnet- | sed -E 's/^.*(subnet-[a-z0-9]+).*$/\1/'`; do aws ec2 delete-subnet --subnet-id=$i; done
+
+# Detach internet gateways
+for i in `aws ec2 describe-internet-gateways --filters Name=attachment.vpc-id,Values="${vpcId}" | grep igw- | sed -E 's/^.*(igw-[a-z0-9]+).*$/\1/'`; do aws ec2 detach-internet-gateway --internet-gateway-id=$i --vpc-id=$vpcId; done
+
+# Delete internet gateways
+for i in `aws ec2 describe-internet-gateways --filters Name=attachment.vpc-id,Values="${vpcId}" | grep igw- | sed -E 's/^.*(igw-[a-z0-9]+).*$/\1/'`; do aws ec2 delete-internet-gateway --internet-gateway-id=$i; done
+
+# Delete security groups (ignore message about being unable to delete default security group)
+for i in `aws ec2 describe-security-groups --filters Name=vpc-id,Values="${vpcId}" | grep sg- | sed -E 's/^.*(sg-[a-z0-9]+).*$/\1/' | sort | uniq`; do aws ec2 delete-security-group --group-id $i || true; done
+
+# Delete the VPC
+aws ec2 delete-vpc --vpc-id ${vpcId}
