d887981bed11a1214f8e4f5d91be287a6cb70453
MandyMY <wangmychn@gmail.com>
2019-02-07 22:20:44 -0500

    fix cannot delete vpc

--- infrastructure/aws/cloudformation/csye6225-aws-networking-teardown.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-networking-teardown.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
+sh #!/bin/bash
 # delete-aws-vpc
 
 aws ec2 describe-vpcs
 
 echo "Enter your vpc-id and press [ENTER]: "
 read vpcId
+
 # rmove vpc with cidr block /16
 
 # Delete subnets
+
 for i in `aws ec2 describe-subnets --filters Name=vpc-id,Values="${vpcId}" | grep subnet- | sed -E 's/^.*(subnet-[a-z0-9]+).*$/\1/'`; do aws ec2 delete-subnet --subnet-id=$i; done
 
 # Detach internet gateways
@@ -17,7 +19,12 @@ for i in `aws ec2 describe-internet-gateways --filters Name=attachment.vpc-id,Va
 for i in `aws ec2 describe-internet-gateways --filters Name=attachment.vpc-id,Values="${vpcId}" | grep igw- | sed -E 's/^.*(igw-[a-z0-9]+).*$/\1/'`; do aws ec2 delete-internet-gateway --internet-gateway-id=$i; done
 
 # Delete security groups (ignore message about being unable to delete default security group)
-for i in `aws ec2 describe-security-groups --filters Name=vpc-id,Values="${vpcId}" | grep sg- | sed -E 's/^.*(sg-[a-z0-9]+).*$/\1/' | sort | uniq`; do aws ec2 delete-security-group --group-id $i || true; done
+#for i in `aws ec2 describe-security-groups --filters Name=vpc-id,Values="${vpcId}" | grep sg- | sed -E 's/^.*(sg-[a-z0-9]+).*$/\1/' | sort | uniq`; do aws ec2 delete-security-group --group-id $i || true; done
 
-# Delete the VPC
+# Delete route tables
+for i in `aws ec2 describe-route-tables --filters Name=vpc-id,Values="${vpcId}" | grep rtb- | sed -E 's/^.*(rtb-[a-z0-9]+).*$/\1/'`; do aws ec2 delete-route-table --route-table-id=$i; done
+
+# Delete the VPCcdc
 aws ec2 delete-vpc --vpc-id ${vpcId}
+
+
