e6096238233bea2e23f07e88b4c92726106cb587
zhichao pan <pan.z@husky.neu.edu>
2019-03-19 22:33:43 -0400

    feat: change log

--- webapp/services/log.js
+++ webapp/services/log.js
@@ -1,5 +1,6 @@
 const winston = require("winston");
-const logDir = "./../logs/";
+const fs = require("fs");
+const logDir = "./logs/";
 const file = "webapp.log";
 if (!fs.existsSync(logDir)) {
   fs.mkdirSync(logDir);
