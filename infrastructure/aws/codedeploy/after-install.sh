ad0c89cfd804f32dd1b1e2fcdbb9445481053a52
zhichao pan <pan.z@husky.neu.edu>
2019-03-21 18:47:02 -0400

    feat: update cloud watch

--- infrastructure/aws/codedeploy/after-install.sh
+++ infrastructure/aws/codedeploy/after-install.sh
@@ -5,4 +5,5 @@ sudo pm2 stop all
 sudo pm2 delete all
 sudo npm install
 sudo cp /home/centos/env .env
+sudo cp amazon-cloudwatch-agent-schema.json /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 
